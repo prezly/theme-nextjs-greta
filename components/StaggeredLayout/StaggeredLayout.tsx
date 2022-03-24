8c7611aef2735f483623b87dc2bd03f5e6f256f5
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-03-24 09:56:42 +0100

    Setting up the StaggeredLayout props and automatically check the default column

--- components/StaggeredLayout/StaggeredLayout.tsx
+++ components/StaggeredLayout/StaggeredLayout.tsx
@@ -1,5 +1,5 @@
 import type { CSSProperties, PropsWithChildren } from 'react';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 type PropsType = {
     breakpointsColumn:
@@ -19,6 +19,18 @@ const DEFAULT_COLUMNS = 2;
 
 function StaggeredLayout(props: PropsWithChildren<PropsType>) {
     const [columnCount, setColumnCount] = useState(0);
+
+    const setColumnDefault = useCallback(
+        () =>
+            typeof props.breakpointsColumn === 'object'
+                ? setColumnCount(props.breakpointsColumn.default)
+                : setColumnCount(props.breakpointsColumn || DEFAULT_COLUMNS),
+        [props.breakpointsColumn],
+    );
+
+    useEffect(() => {
+        setColumnDefault();
+    }, [setColumnDefault]);
 
     return (
         <div style={props.style} className={classNameOutput}>
