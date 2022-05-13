c8c3bbf6d600525318f911fdbf517e192c3f9cfb
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-13 11:28:55 +0300

    Use new `story.referenced_entities` property instead of `useEmbedStory()`
    See https://github.com/prezly/theme-nextjs-greta/pull/14#discussion_r872092534

--- components/SlateRenderer/components/StoryBookmark.tsx
+++ components/SlateRenderer/components/StoryBookmark.tsx
@@ -1,17 +1,18 @@
 import { Elements } from '@prezly/content-renderer-react-js';
 import type { StoryBookmarkNode } from '@prezly/story-content-format';
-import { useEmbedStory } from '@prezly/theme-kit-nextjs';
+import { useCurrentStory } from '@prezly/theme-kit-nextjs';
 
 interface Props {
     node: StoryBookmarkNode;
 }
 
 export function StoryBookmark({ node }: Props) {
-    const embedStory = useEmbedStory(node.story.uuid);
+    const story = useCurrentStory();
+    const referencedStory = story?.referenced_entities?.stories?.[node.story.uuid];
 
-    if (!embedStory) {
+    if (!referencedStory) {
         return null;
     }
 
-    return <Elements.StoryBookmark node={node} storyOEmbedInfo={embedStory.oembed} />;
+    return <Elements.StoryBookmark node={node} storyOEmbedInfo={referencedStory} />;
 }
