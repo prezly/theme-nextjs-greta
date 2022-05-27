9588fae069e3edfc00f6a357eb1bb815657f7fb8
Oleg Semyonov <oleg@prezly.com>
2022-05-27 12:23:21 +0400

    Replace `react-use` with `@react-hookz/web`

--- hooks/useDevice.ts
+++ hooks/useDevice.ts
@@ -1,12 +1,12 @@
-import { useMedia } from 'react-use';
+import { useMediaQuery } from '@react-hookz/web';
 
 const BREAKPOINT_TABLET = 414;
 const BREAKPOINT_DESKTOP = 768;
 
 export function useDevice() {
-    const isMobile = useMedia(`(max-width: ${BREAKPOINT_TABLET}px)`, true);
-    const isTablet = useMedia(`(max-width: ${BREAKPOINT_DESKTOP}px)`, false);
-    const isDesktop = useMedia(`(min-width: ${BREAKPOINT_DESKTOP + 1}px)`, false);
+    const isMobile = useMediaQuery(`(max-width: ${BREAKPOINT_TABLET}px)`, true) ?? true;
+    const isTablet = useMediaQuery(`(max-width: ${BREAKPOINT_DESKTOP}px)`, false);
+    const isDesktop = useMediaQuery(`(min-width: ${BREAKPOINT_DESKTOP + 1}px)`, false);
 
     return {
         isMobile,
