7f3473319c1ecfa6758a6509598ea4610cabdce7
Gichohi-Simon <njogugichohi56@gmail.com>
2022-08-10 11:30:26 +0300

    applied approved changes from bea

--- components/ContactCard/ContactCard.tsx
+++ components/ContactCard/ContactCard.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import type { ReactNode } from 'react';
 
 import { useDevice } from '@/hooks';
-import { IconEmail, IconFacebook, IconGlobe, IconPhone, IconTwitter } from '@/icons';
+import { IconEmail, IconFacebook, IconGlobe, IconPhone, IconTwitter } from '@prezly/icons';
 
 import type { Contact } from './types';
 import { getSocialHandles } from './utils';
@@ -15,47 +15,31 @@ interface Props {
     isCompact?: boolean;
     renderAvatar: ({ className }: { className: string }) => ReactNode;
 }
-
 function ContactCard({ className, contact, isCompact = false, renderAvatar }: Props) {
     const device = useDevice();
     const { name, description, company, email, phone, mobile, website } = contact;
     const { facebook, twitter } = getSocialHandles(contact);
     const subtitle = description && company ? `${description}, ${company}` : description;
-
     return (
         <div
             id={`contact-${contact.uuid}`}
             className={classNames(styles.container, className, {
                 [styles.compact]: isCompact || device.isMobile,
-                [styles.tablet]: isCompact || device.isTablet,
             })}
         >
-            <div className={styles.content}>
-                <div className={styles.contentTitle}>
-                    {renderAvatar({ className: styles.avatar })}
-                    <div>
-                        <h4 className={styles.name}>{name}</h4>
-                        {subtitle && <h5 className={styles.position}>{subtitle}</h5>}
-                    </div>
+            <div className={styles.contentTitle}>
+                {renderAvatar({ className: styles.avatar })}
+                <div>
+                    <h4 className={styles.name}>{name}</h4>
+                    {subtitle && <h5 className={styles.position}>{subtitle}</h5>}
                 </div>
-
-                <div className={styles.links}>
+            </div>
+            <div className={styles.links}>
+                <div className={styles.primaryContacts}>
                     {email && (
                         <a href={`mailto:${email}`} className={styles.link}>
                             <IconEmail width={16} height={16} className={styles.icon} />
                             <span className={styles.linkText}>{email}</span>
-                        </a>
-                    )}
-                    {website && (
-                        <a href={website} className={styles.link}>
-                            <IconGlobe width={16} height={16} className={styles.icon} />
-                            <span className={styles.linkText}>{website}</span>
-                        </a>
-                    )}
-                    {mobile && (
-                        <a href={`tel:${mobile}`} className={styles.link}>
-                            <IconPhone width={16} height={16} className={styles.icon} />
-                            <span className={styles.linkText}>{mobile}</span>
                         </a>
                     )}
                     {phone && (
@@ -64,16 +48,30 @@ function ContactCard({ className, contact, isCompact = false, renderAvatar }: Pr
                             <span className={styles.linkText}>{phone}</span>
                         </a>
                     )}
-                    {twitter && (
-                        <a href={`https://twitter.com/${twitter}`} className={styles.link}>
-                            <IconTwitter width={16} height={16} className={styles.icon} />
-                            <span className={styles.linkText}>{`@${twitter}`}</span>
+                    {mobile && (
+                        <a href={`tel:${mobile}`} className={styles.link}>
+                            <IconPhone width={16} height={16} className={styles.icon} />
+                            <span className={styles.linkText}>{mobile}</span>
                         </a>
                     )}
+                    {website && (
+                        <a href={website} className={styles.link}>
+                            <IconGlobe width={16} height={16} className={styles.icon} />
+                            <span className={styles.linkText}>{website}</span>
+                        </a>
+                    )}
+                </div>
+                <div className={styles.socials}>
                     {facebook && (
                         <a href={`https://facebook.com/${facebook}`} className={styles.link}>
                             <IconFacebook width={16} height={16} className={styles.icon} />
                             <span className={styles.linkText}>{facebook}</span>
+                        </a>
+                    )}
+                    {twitter && (
+                        <a href={`https://twitter.com/${twitter}`} className={styles.link}>
+                            <IconTwitter width={16} height={16} className={styles.icon} />
+                            <span className={styles.linkText}>{`@${twitter}`}</span>
                         </a>
                     )}
                 </div>
@@ -81,5 +79,4 @@ function ContactCard({ className, contact, isCompact = false, renderAvatar }: Pr
         </div>
     );
 }
-
 export default ContactCard;
