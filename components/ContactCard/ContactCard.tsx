06466e6a328978db2fdb493060afb8282709b838
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-01-19 14:54:24 +0200

    [DEV-9838] Adapt code to the updated `ContactNode` model

--- components/ContactCard/ContactCard.tsx
+++ components/ContactCard/ContactCard.tsx
@@ -11,25 +11,27 @@ import type { ReactNode } from 'react';
 
 import { useDevice } from '@/hooks';
 
-import type { Contact } from './types';
+import type { ContactInfo } from './types';
 import { getSocialHandles } from './utils';
 
 import styles from './ContactCard.module.scss';
+import { ContactNode } from '@prezly/story-content-format';
 
 interface Props {
     className?: string;
-    contact: Contact;
+    contactInfo: ContactInfo;
     isCompact?: boolean;
     renderAvatar: ({ className }: { className: string }) => ReactNode;
+    uuid: ContactNode['uuid'];
 }
-function ContactCard({ className, contact, isCompact = false, renderAvatar }: Props) {
+function ContactCard({ className, contactInfo, isCompact = false, renderAvatar, uuid }: Props) {
     const device = useDevice();
-    const { name, description, company, email, phone, mobile, website } = contact;
-    const { facebook, twitter } = getSocialHandles(contact);
+    const { name, description, company, email, phone, mobile, website } = contactInfo;
+    const { facebook, twitter } = getSocialHandles(contactInfo);
     const subtitle = description && company ? `${description}, ${company}` : description;
     return (
         <div
-            id={`contact-${contact.uuid}`}
+            id={`contact-${uuid}`}
             className={classNames(styles.container, className, {
                 [styles.compact]: isCompact || device.isMobile,
             })}
