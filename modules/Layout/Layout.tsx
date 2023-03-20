05d29826a8d024b933dac8296d5c164547389292
Oleg Semyonov <oleg@prezly.com>
2023-03-20 13:33:19 +0400

    Port back basic UI components

--- modules/Layout/Layout.tsx
+++ modules/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Analytics, useAnalyticsContext } from '@prezly/analytics-nextjs';
 import { PageSeo, useNewsroom, useNewsroomContext } from '@prezly/theme-kit-nextjs';
-import { LoadingBar, NotificationsBar, ScrollToTopButton } from '@prezly/themes-ui-components';
+import { NotificationsBar } from '@prezly/themes-ui-components';
 import dynamic from 'next/dynamic';
 import { Router } from 'next/router';
 import type { PropsWithChildren } from 'react';
 import { useEffect, useState } from 'react';
+
+import { LoadingBar, ScrollToTopButton } from '@/ui';
 
 import Boilerplate from './Boilerplate';
 import Branding from './Branding';
