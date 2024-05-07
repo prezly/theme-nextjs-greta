dbfe90f2d7422caf50bf60c25ca2b1dc3709f5b9
kudlajz <kudlajzzz@gmail.com>
2024-05-07 15:00:35 +0200

    Optimize story card images

--- components/StoryImage/StoryImage.tsx
+++ components/StoryImage/StoryImage.tsx
@@ -1,9 +1,9 @@
 import type { AlgoliaStory } from '@prezly/theme-kit-core';
 import { useNewsroom } from '@prezly/theme-kit-nextjs';
-import Image from '@prezly/uploadcare-image';
+import UploadcareImage from '@uploadcare/nextjs-loader';
 import classNames from 'classnames';
 
-import { type CardSize, getCardImageSizes } from '@/utils';
+import { type CardSize, getCardImageSizes, getUploadcareFile } from '@/utils';
 import type { StoryWithImage } from 'types';
 
 import { getStoryThumbnail } from './lib';
@@ -20,31 +20,33 @@ type Props = {
 function StoryImage({ story, size, className, placeholderClassName }: Props) {
     const { name, newsroom_logo: logo } = useNewsroom();
     const image = getStoryThumbnail(story);
+    const imageFile = getUploadcareFile(image);
 
-    if (image) {
+    if (imageFile) {
         return (
-            <Image
-                imageDetails={image}
-                alt={story.title}
-                layout="fill"
-                objectFit="cover"
-                containerClassName={classNames(styles.imageContainer, className)}
-                className={styles.image}
-                sizes={getCardImageSizes(size)}
-            />
+            <div className={classNames(styles.imageContainer, className)}>
+                <UploadcareImage
+                    alt={story.title}
+                    className={styles.image}
+                    sizes={getCardImageSizes(size)}
+                    src={imageFile.cdnUrl}
+                    fill
+                />
+            </div>
         );
     }
 
+    const logoFile = getUploadcareFile(logo);
+
     return (
         <span className={classNames(styles.placeholder, placeholderClassName)}>
-            {logo && (
-                <Image
-                    imageDetails={logo}
-                    layout="fill"
-                    objectFit="contain"
+            {logoFile && (
+                <UploadcareImage
                     alt="No image"
                     className={classNames(styles.imageContainer, styles.placeholderLogo, className)}
-                    sizes={{ default: 256 }}
+                    src={logoFile.cdnUrl}
+                    width={320}
+                    height={56}
                 />
             )}
             {!logo && name}
