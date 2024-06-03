dd0b77cde5bf86990511a1834abfec5aae3eb746
kudlajz <kudlajzzz@gmail.com>
2024-06-03 14:52:18 +0200

    Implement mask param hook and hide cookie bar if it's present

---
+++ hooks/useMaskParam.ts
@@ -0,0 +1,19 @@
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
+
+export function useMaskParam(): boolean {
+    const searchParams = useSearchParams();
+    const mask = searchParams.get('mask');
+
+    return useMemo(() => {
+        if (!mask) {
+            return false;
+        }
+
+        try {
+            return Boolean(JSON.parse(mask));
+        } catch (_) {
+            return false;
+        }
+    }, [mask]);
+}
