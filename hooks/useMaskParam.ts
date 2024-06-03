924daa563dfbfb4dafaa95d546cfc3de648458e3
kudlajz <kudlajzzz@gmail.com>
2024-06-03 15:02:01 +0200

    Get rid of useMemo

--- hooks/useMaskParam.ts
+++ hooks/useMaskParam.ts
@@ -1,19 +1,16 @@
 import { useSearchParams } from 'next/navigation';
-import { useMemo } from 'react';
 
 export function useMaskParam(): boolean {
     const searchParams = useSearchParams();
     const mask = searchParams.get('mask');
 
-    return useMemo(() => {
-        if (!mask) {
-            return false;
-        }
+    if (!mask) {
+        return false;
+    }
 
-        try {
-            return Boolean(JSON.parse(mask));
-        } catch (_) {
-            return false;
-        }
-    }, [mask]);
+    try {
+        return Boolean(JSON.parse(mask));
+    } catch {
+        return false;
+    }
 }
